1c2225c5355ffc89c27ba45255be4c04fbcc3aad
David Kim <davidhkim1102@gmail.com>
2020-10-30 13:50:24 -0700

    kyle's sins have been unforgiven

--- animation.py
+++ animation.py
@@ -1,252 +1,251 @@
 import time
 import os
 from random import randrange
-#adding a comment to test editing on INteliJ
+import sys
+
+
+# adding a comment to test editing on INteliJ
 def menuAni():
-    #terminal print commands
-  ANSI_CLEAR_SCREEN = u"\u001B[2J"
-  ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
-  RESET_COLOR = u"\u001B[0m\u001B[2D"
+    # terminal print commands
+    ANSI_CLEAR_SCREEN = u"\u001B[2J"
+    ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
+    RESET_COLOR = u"\u001B[0m\u001B[2D"
 
-  CBLACK  = '\33[30m'
-  CRED    = '\33[31m'
-  CGREEN  = '\33[32m'
-  CYELLOW = '\33[33m'
-  CBLUE   = '\33[34m'
-  CVIOLET = '\33[35m'
-  CBEIGE  = '\33[36m'
-  CWHITE  = '\33[37m'
+    CBLACK = '\33[30m'
+    CRED = '\33[31m'
+    CGREEN = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE = '\33[36m'
+    CWHITE = '\33[37m'
 
-  colorBar = CGREEN #color of the loading bar
-  possibleColor = [ CBLACK, CRED, CGREEN, CYELLOW, CBLUE, CVIOLET, CBEIGE, CWHITE]
+    colorBar = CGREEN  # color of the loading bar
+    possibleColor = [CBLACK, CRED, CGREEN, CYELLOW, CBLUE, CVIOLET, CBEIGE, CWHITE]
 
-  chess = [
-  "                            ",
-  "       _                    ",
-  "   ___| |__   ___  ___ ___  ",
-  "  / __| '_ \ / _ \/ __/ __| ",
-  " | (__| | | |  __/\__ \__ \ ",
-  "  \___|_| |_|\___||___/___/ "
-  ]
+    chess = [
+        "                            ",
+        "       _                    ",
+        "   ___| |__   ___  ___ ___  ",
+        "  / __| '_ \ / _ \/ __/ __| ",
+        " | (__| | | |  __/\__ \__ \ ",
+        "  \___|_| |_|\___||___/___/ "
+    ]
 
-  pawn =[
-  "    _    ",
-  "   (_)   ",
-  "  (___)  ",
-  "  _|_|_  ",
-  " (_____) ",
-  " /_____\ "
-  ]
+    pawn = [
+        "    _    ",
+        "   (_)   ",
+        "  (___)  ",
+        "  _|_|_  ",
+        " (_____) ",
+        " /_____\ "
+    ]
 
-  def printTitle():
-    for i in range(len(chess)):
-      print(pawn[i] + chess[i] + pawn[i])
+    def printTitle():
+        for i in range(len(chess)):
+            print(pawn[i] + chess[i] + pawn[i])
 
-  length = 16
-  loading = [] #this is the list which creates the loading animation
+    length = 16
+    loading = []  # this is the list which creates the loading animation
 
-  def printLoading(postition):
-    space = (postition-1) * "   " 
-    print(*loading, end = "")#this prints the whole list of the loading bar and 
-    print(space + "|")#this is the symbol for the final vertical bar, denotes fully loaded when reached
-     
-    loading.append(colorBar + "☺☻") #appends another mark onto the loading bar
+    def printLoading(postition):
+        space = (postition - 1) * "   "
+        print(*loading, end="")  # this prints the whole list of the loading bar and
+        print(space + "|")  # this is the symbol for the final vertical bar, denotes fully loaded when reached
 
-  for i in range(0,length):
-    #c.cursor(0)
-    print(RESET_COLOR,end = "") #to stop coloring after the ocean, without this line then everything is highlighted
-    print(ANSI_HOME_CURSOR,end = "")#to clear the screen print in the same top left corner every time
+        loading.append(colorBar + "☺☻")  # appends another mark onto the loading bar
 
-    print(possibleColor[randrange(len(possibleColor))],end = "") #chooses random color for the title 
-    printTitle() #printing title 
-    backwards = length - i #to help with the placement of the vertical end line
-    printLoading(backwards)#prints the loading bar under the title
+    for i in range(0, length):
+        # c.cursor(0)
+        print(RESET_COLOR, end="")  # to stop coloring after the ocean, without this line then everything is highlighted
+        print(ANSI_HOME_CURSOR, end="")  # to clear the screen print in the same top left corner every time
 
-    time.sleep(.1) #to diffrentiate the color changes
+        print(possibleColor[randrange(len(possibleColor))], end="")  # chooses random color for the title
+        printTitle()  # printing title
+        backwards = length - i  # to help with the placement of the vertical end line
+        printLoading(backwards)  # prints the loading bar under the title
 
-  os.system('clear')
-  print(CWHITE, end = "")
-  printTitle() #printing title 
-  print(*loading, end = "")#prints the whole bar
-  print("|")#hard coding reached the end
-  print(CWHITE)
+        time.sleep(.1)  # to diffrentiate the color changes
+
+    #os.system('clear')
+    print(CWHITE, end="")
+    printTitle()  # printing title
+    print(*loading, end="")  # prints the whole bar
+    print("|")  # hard coding reached the end
+    print(CWHITE)
+
 
 def endAni():
-  ANSI_CLEAR_SCREEN = u"\u001B[2J"
-  ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
-  RESET_COLOR = u"\u001B[0m\u001B[2D"
+    ANSI_CLEAR_SCREEN = u"\u001B[2J"
+    ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
+    RESET_COLOR = u"\u001B[0m\u001B[2D"
 
-  #coloring the text  
+    # coloring the text
 
-  CBLACK  = '\33[30m'
-  CRED    = '\33[31m'
-  CGREEN  = '\33[32m'
-  CYELLOW = '\33[33m'
-  CBLUE   = '\33[34m'
-  CVIOLET = '\33[35m'
-  CBEIGE  = '\33[36m'
-  CWHITE  = '\33[37m'
+    CBLACK = '\33[30m'
+    CRED = '\33[31m'
+    CGREEN = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE = '\33[36m'
+    CWHITE = '\33[37m'
 
-  CREDBG    = '\33[41m' #highlighting background
-  CWHITEBG  = '\33[47m'
-  CBLACKBG  = '\33[40m'
+    CREDBG = '\33[41m'  # highlighting background
+    CWHITEBG = '\33[47m'
+    CBLACKBG = '\33[40m'
 
-  possibleColor = [ CBLACK, CRED, CGREEN, CYELLOW, CBLUE, CVIOLET, CBEIGE, CWHITE]
+    possibleColor = [CBLACK, CRED, CGREEN, CYELLOW, CBLUE, CVIOLET, CBEIGE, CWHITE]
 
-  pawn =[
-    "         ",
-    "         ",
-    "         ",
-    "         ",
-    "         ",
-    "    _    ",
-    "   (_)   ",
-    "  (___)  ",
-    "  _|_|_  ",
-    " (_____) ",
-    " /_____\ "
-  ]
-  rook =[
-    "          ",
-    "          ",
-    " _  _  _  ",
-    "| || || | ",
-    "|_______| ",
-    "\__ ___ / ",
-    " |___|_|  ",
-    " |_|___|  ",
-    " |___|_|  ",
-    "(_______) ",
-    "/_______\ "
-  ]
+    pawn = [
+        "         ",
+        "         ",
+        "         ",
+        "         ",
+        "         ",
+        "    _    ",
+        "   (_)   ",
+        "  (___)  ",
+        "  _|_|_  ",
+        " (_____) ",
+        " /_____\ "
+    ]
+    rook = [
+        "          ",
+        "          ",
+        " _  _  _  ",
+        "| || || | ",
+        "|_______| ",
+        "\__ ___ / ",
+        " |___|_|  ",
+        " |_|___|  ",
+        " |___|_|  ",
+        "(_______) ",
+        "/_______\ "
+    ]
 
-  knight = [
-    "           ",
-    "           ",
-    "   ^^__    ",
-    "  /  - \_  ",
-    "<|    __<  ",
-    "<|    \    ",
-    "<|     \   ",
-    "<|______\  ",
-    " _|____|_  ",
-    "(________) ",
-    "/________\ "
+    knight = [
+        "           ",
+        "           ",
+        "   ^^__    ",
+        "  /  - \_  ",
+        "<|    __<  ",
+        "<|    \    ",
+        "<|     \   ",
+        "<|______\  ",
+        " _|____|_  ",
+        "(________) ",
+        "/________\ "
 
-  ]
+    ]
 
-  bishop = [
-    "          ",
-    "   _O     ",
-    "  / //\   ",
-    " {     }  ",
-    "  \___/   ",
-    "  (___)   ",
-    "   |_|    ",
-    "  /   \   ",
-    " (_____)  ",
-    "(_______) ",
-    "/_______\ "
-  ]
-  queen = [
-    "   _()_    ",
-    " _/____\_  ",
-    " \      /  ",
-    "  \____/   ",
-    "  (____)   ",
-    "   |  |    ",
-    "   |__|    ",
-    "  /    \   ",
-    " (______)  ",
-    "(________) ",
-    "/________\ "
-  ]
+    bishop = [
+        "          ",
+        "   _O     ",
+        "  / //\   ",
+        " {     }  ",
+        "  \___/   ",
+        "  (___)   ",
+        "   |_|    ",
+        "  /   \   ",
+        " (_____)  ",
+        "(_______) ",
+        "/_______\ "
+    ]
+    queen = [
+        "   _()_    ",
+        " _/____\_  ",
+        " \      /  ",
+        "  \____/   ",
+        "  (____)   ",
+        "   |  |    ",
+        "   |__|    ",
+        "  /    \   ",
+        " (______)  ",
+        "(________) ",
+        "/________\ "
+    ]
 
-  king = [
-    "   _::_    ",
-    " _/____\_  ",
-    " \      /  ",
-    "  \____/   ",
-    "  (____)   ",
-    "   |  |    ",
-    "   |__|    ",
-    "  /    \   ",
-    " (______)  ",
-    "(________) ",
-    "/________\ "
-  ]
+    king = [
+        "   _::_    ",
+        " _/____\_  ",
+        " \      /  ",
+        "  \____/   ",
+        "  (____)   ",
+        "   |  |    ",
+        "   |__|    ",
+        "  /    \   ",
+        " (______)  ",
+        "(________) ",
+        "/________\ "
+    ]
 
-  attackPiece = [pawn, rook, knight, bishop, queen]
+    attackPiece = [pawn, rook, knight, bishop, queen]
 
-  length = 24
-  spaceF = 0
-  lengthOfRect = length + 24
-  kingColor = CRED
-  attackColor = CBEIGE
+    length = 24
+    spaceF = 0
+    lengthOfRect = length + 24
+    kingColor = CRED
+    attackColor = CBEIGE
 
-  q = randrange(len(attackPiece))
+    q = randrange(len(attackPiece))
 
-  def resetScene():
-    print(RESET_COLOR,end = "")
-    print(ANSI_HOME_CURSOR,end = "")
-    
-  def printScene(postition,backwards,attackColor, kingColor,attack):
-    spaceF = postition * " "
-    spaceB = backwards * " " 
-    for i in range(len(pawn)):
-      print(spaceF + attackColor + attack[i] + spaceB + kingColor + king[i])
+    def resetScene():
+        print(RESET_COLOR, end="")
+        print(ANSI_HOME_CURSOR, end="")
 
-  def print1by1(text, color, delay=0.1):
-      for c in text:
-          sys.stdout.write(color + c)
-          sys.stdout.flush()
-          time.sleep(delay)
-      print
+    def printScene(postition, backwards, attackColor, kingColor, attack):
+        spaceF = postition * " "
+        spaceB = backwards * " "
+        for i in range(len(pawn)):
+            print(spaceF + attackColor + attack[i] + spaceB + kingColor + king[i])
 
-  """
+    def print1by1(text, color, delay=0.1):
+        for c in text:
+            sys.stdout.write(color + c)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print
+
+    """
   def imageTest(q):
     for i in range(len(attackPiece[q])):
       print(attackPiece[q][i]+"|")
   """
 
-  def move (y, x):
-      print("\033[%d;%dH" % (y, x))
-      
-  def printMiddle(message,background):
-    front = lengthOfRect - len(message)
-    front = front/2
-    front = round(front)
-    space = " " * front
-    print("\n")
-    print(background + space, end = "")
-    print1by1(message,CREDBG)
-    move(11,0)
-    print(u"\u001B[0m")
-    #print(background + ANSI_HOME_CURSOR + background)
+    def move(y, x):
+        print("\033[%d;%dH" % (y, x))
 
-  print(RESET_COLOR,end = "")
-  print(ANSI_HOME_CURSOR)
-  for i in range(50):
-    print("\n")
+    def printMiddle(message, background):
+        front = lengthOfRect - len(message)
+        front = front / 2
+        front = round(front)
+        space = " " * front
+        print("\n")
+        print(background + space, end="")
+        print1by1(message, CREDBG)
+        move(11, 0)
+        print(u"\u001B[0m")
+        # print(background + ANSI_HOME_CURSOR + background)
 
+    print(RESET_COLOR, end="")
+    print(ANSI_HOME_CURSOR)
+    for i in range(50):
+        print("\n")
 
-  for i in range(0,length):
+    for i in range(0, length):
+        resetScene()
+        print(CBLACK, end="")  # color the background
+        backwards = length - i  # to help with the placement of the vertical end line
+        printScene(i, backwards, attackColor, kingColor, attackPiece[q])
+        time.sleep(.1)  # to diffrentiate the color changes
+
     resetScene()
-    print(CBLACK,end = "") #color the background 
-    backwards = length - i #to help with the placement of the vertical end line
-    printScene(i,backwards,attackColor,kingColor,attackPiece[q])
-    time.sleep(.1) #to diffrentiate the color changes
 
-  resetScene()
+    for i in range(len(king)):  # all all red
+        print(CREDBG + " " * lengthOfRect)
 
-  for i in range(len(king)):# all all red
-    print(CREDBG+" "*lengthOfRect)
-
-  time.sleep(.1) #to diffrentiate the color changes
-  resetScene()
-  message = "The King has been Captured!"
-  printMiddle(message,CREDBG)
-      
-    
-
-  
+    time.sleep(.1)  # to diffrentiate the color changes
+    resetScene()
+    message = "The King has been Captured!"
+    printMiddle(message, CREDBG)
--- replaygame.py
+++ replaygame.py
@@ -1,3 +1,8 @@
+import math
+from main import *
+from zwhitepersp import *
+from zblackpersp import *
+
 # Read the replayfile
 replayfile = open("savereplay.txt").read()
 
@@ -10,19 +15,21 @@ turncounter = 0
 lastturn = False
 autoplay = False
 
+
 # Turn printing function
 def turnprint(currentcounter):
-  turn = currentcounter / 10
-  turn = math.floor(turn + 1)
-  strturn = str(int(turn))
-  if currentcounter % 2 == 0:
-    global turnprintx
-    turnprintx = "\nNext Turn: " + strturn + " Black\n\n"
-    print("\nNext Turn: " + strturn + " Black")
-  else:
-    global turnprint
-    turnprintx = "\nNext Turn: " + strturn + " White\n"
-    print("\nNext Turn: " + strturn + " White")
+    turn = currentcounter / 10
+    turn = math.floor(turn + 1)
+    strturn = str(int(turn))
+    if currentcounter % 2 == 0:
+        global turnprintx
+        turnprintx = "\nNext Turn: " + strturn + " Black\n\n"
+        print("\nNext Turn: " + strturn + " Black")
+    else:
+        global turnprint
+        turnprintx = "\nNext Turn: " + strturn + " White\n"
+        print("\nNext Turn: " + strturn + " White")
+
 
 Keylist = ["A", "B", "C", "D", "E", "F"]
 Keycheck = ""
@@ -33,126 +40,125 @@ indexing = -1
 Key = input("\nWhat is your game key?\n")
 
 if len(Key) != 6:
-  print("Please enter a valid game key.\n")
-  mainmenu(Key,key,replaying,exited,settingexit,whitecolor,blackcolor,colorset)
+    print("Please enter a valid game key.\n")
+    mainmenu(Key, key, replaying, exited, settingexit, whitecolor, blackcolor, colorset)
 
 for letter in replayfile:
-  indexing += 1
-  Keylist.pop(0)
-  Keylist.append(replayfile[indexing])
-  for keything in Keylist:
-    Keycheck = Keylist[0] + Keylist[1] + Keylist[2] + Keylist[3] + Keylist[4] + Keylist[5]
-  if Keycheck == Key:
-    index = indexing + 1
-    Keychecked = True
-    replaying = True
-    break
-  else:
-    Keycheck = ""
+    indexing += 1
+    Keylist.pop(0)
+    Keylist.append(replayfile[indexing])
+    for keything in Keylist:
+        Keycheck = Keylist[0] + Keylist[1] + Keylist[2] + Keylist[3] + Keylist[4] + Keylist[5]
+    if Keycheck == Key:
+        index = indexing + 1
+        Keychecked = True
+        replaying = True
+        break
+    else:
+        Keycheck = ""
 
-if Keychecked == False:
-  print("Please enter a valid game key.\n")
-  mainmenu(Key,key,replaying,exited,settingexit,whitecolor,blackcolor,colorset)
+if not Keychecked:
+    print("Please enter a valid game key.\n")
+    mainmenu(Key, key, replaying, exited, settingexit, whitecolor, blackcolor, colorset)
 
-os.system("clear")
 print("\nNext Turn: 1 White\n\n")
-whitepersp(whitecolor,blackcolor)
+whitepersp(whitecolor, blackcolor,board)
 
 while lastturn == False:
-  if autoplay == False:
-    forward = input("\nType 'n' or 'next' to continue, 'f' or 'flip' to change perspectives, 'e' or 'exit' to exit, 'a #' or 'auto #' to automatically play the rest of the game, with each move being made at the specified interval of time.\n")
-    forward = forward.lower()
-  else:
-    forward = "n"
-    time.sleep(autotimer)
-#Code for moving through replay by user input
-  if forward == "e" or forward == "exit":
-    break
+    if autoplay == False:
+        forward = input(
+            "\nType 'n' or 'next' to continue, 'f' or 'flip' to change perspectives, 'e' or 'exit' to exit, 'a #' or 'auto #' to automatically play the rest of the game, with each move being made at the specified interval of time.\n")
+        forward = forward.lower()
+    else:
+        forward = "n"
+        time.sleep(autotimer)
+    # Code for moving through replay by user input
+    if forward == "e" or forward == "exit":
+        break
 
-  elif forward == "f" or forward == "flip":
-    os.system("clear")
-    print(turnprintx)
-    if repersp == "white":
-      repersp = "black"
-      blackpersp(whitecolor,blackcolor)
-    elif repersp == "black":
-      repersp = "white"
-      print("")
-      whitepersp(whitecolor,blackcolor)
+    elif forward == "f" or forward == "flip":
+        #os.system("clear")
+        print(turnprintx)
+        if repersp == "white":
+            repersp = "black"
+            blackpersp(whitecolor, blackcolor, board)
+        elif repersp == "black":
+            repersp = "white"
+            print("")
+            whitepersp(whitecolor, blackcolor, board)
 
-  elif forward == "n" or forward == "next" and lastturn == False:
-    os.system("clear")
-    turnprint(currentcounter)
-    for i in range(5):
-      if currentletter != "#":
-        currentletter = replayfile[currentcounter + index]
-        currentmove = currentmove + currentletter
-        currentcounter += 1
-      else:
-        lastturn = True
-        currentletter = replayfile[currentcounter + index]
-        currentmove = currentmove + currentletter
-        currentcounter += 1
+    elif forward == "n" or forward == "next" and lastturn == False:
+        #os.system("clear")
+        turnprint(currentcounter)
+        for i in range(5):
+            if currentletter != "#":
+                currentletter = replayfile[currentcounter + index]
+                currentmove = currentmove + currentletter
+                currentcounter += 1
+            else:
+                lastturn = True
+                currentletter = replayfile[currentcounter + index]
+                currentmove = currentmove + currentletter
+                currentcounter += 1
 
-    if currentmove != "0-0B " and currentmove != "0-0-B" and currentmove != "0-0W " and currentmove != "0-0-W" and currentmove != "#W   " and currentmove != "#B   ":
-      board[currentmove[3:5]] = board[currentmove[0:2]]
-      board[currentmove[0:2]] = "  "
-      currentmove = ""
-    elif currentmove == "0-0W ":
-      board["e1"] = '  '
-      board["f1"] = 'WR'
-      board["g1"] = 'WK'
-      board["h1"] = '  '
-      currentmove = ""
-    elif currentmove == "0-0B ":
-      board["e8"] = '  '
-      board["f8"] = 'BR'
-      board["g8"] = 'BK'
-      board["h8"] = '  '
-      currentmove = ""
-    elif currentmove == "0-0-W":
-      board["a1"] = '  '
-      board["b1"] = '  '
-      board["c1"] = 'WK'
-      board["d1"] = 'WR'
-      board["e1"] = '  '
-      currentmove = ""
-    elif currentmove == "0-0-B":
-      board["a8"] = '  '
-      board["b8"] = '  '
-      board["c8"] = 'BK'
-      board["d8"] = 'BR'
-      board["e8"] = '  '
-      currentmove = ""
+        if currentmove != "0-0B " and currentmove != "0-0-B" and currentmove != "0-0W " and currentmove != "0-0-W" and currentmove != "#W   " and currentmove != "#B   ":
+            board[currentmove[3:5]] = board[currentmove[0:2]]
+            board[currentmove[0:2]] = "  "
+            currentmove = ""
+        elif currentmove == "0-0W ":
+            board["e1"] = '  '
+            board["f1"] = 'WR'
+            board["g1"] = 'WK'
+            board["h1"] = '  '
+            currentmove = ""
+        elif currentmove == "0-0B ":
+            board["e8"] = '  '
+            board["f8"] = 'BR'
+            board["g8"] = 'BK'
+            board["h8"] = '  '
+            currentmove = ""
+        elif currentmove == "0-0-W":
+            board["a1"] = '  '
+            board["b1"] = '  '
+            board["c1"] = 'WK'
+            board["d1"] = 'WR'
+            board["e1"] = '  '
+            currentmove = ""
+        elif currentmove == "0-0-B":
+            board["a8"] = '  '
+            board["b8"] = '  '
+            board["c8"] = 'BK'
+            board["d8"] = 'BR'
+            board["e8"] = '  '
+            currentmove = ""
 
-    if currentmove == "#W   ":
-      print("\n\nGG, White won!")
-    elif currentmove == "#B   ":
-      print("\n\nGG, Black won!")
-    
-    if repersp == "white":
-      print("\n")
-      whitepersp(whitecolor,blackcolor)
-    elif repersp == "black":
-      print("\n")
-      blackpersp(whitecolor,blackcolor)
+        if currentmove == "#W   ":
+            print("\n\nGG, White won!")
+        elif currentmove == "#B   ":
+            print("\n\nGG, Black won!")
 
-  elif len(forward) >= 2 and forward[0] == "a" and forward[1] == " ":
-    try:
-      autotimer = int(forward[2:])
-      autoplay = True
-    except:
-      print("Please enter a valid time value")
-      
-  elif len(forward) >= 5 and forward[0:4] == "auto" and forward[4] == " ":
-    try:
-      autotimer = int(forward[5:])
-      autoplay = True
-    except:
-      print("Please enter a valid time value")
+        if repersp == "white":
+            print("\n")
+            whitepersp(whitecolor, blackcolor, board)
+        elif repersp == "black":
+            print("\n")
+            blackpersp(whitecolor, blackcolor, board)
 
-  else:
-    print("please input a valid response")
-  
+    elif len(forward) >= 2 and forward[0] == "a" and forward[1] == " ":
+        try:
+            autotimer = int(forward[2:])
+            autoplay = True
+        except:
+            print("Please enter a valid time value")
 
-mainmenu(Key,key,replaying,exited,settingexit,whitecolor,blackcolor,colorset)
+    elif len(forward) >= 5 and forward[0:4] == "auto" and forward[4] == " ":
+        try:
+            autotimer = int(forward[5:])
+            autoplay = True
+        except:
+            print("Please enter a valid time value")
+
+    else:
+        print("please input a valid response")
+
+mainmenu(Key, key, replaying, exited, settingexit, whitecolor, blackcolor, colorset)
